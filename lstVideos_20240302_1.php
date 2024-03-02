021b2ad3feb5e906e7981f58383b78dfd66a8977
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2024-03-02 20:05:53 -0300

    Add files via upload

---
+++ lstVideos_20240302_1.php
@@ -0,0 +1,19 @@
+<?php
+	// Separamos a conexão para poder usar mais vezes
+	include "sqliteCon.php";
+	// Constrói o SQL, ligando logicamente ao parâmetro
+	$sql = "SELECT * FROM tbVideos ";
+	// Bloco com previsão de exceção
+	try{
+		$stmt = $conn->prepare($sql);
+		$res = $stmt->execute();
+		echo "<table border=1 cellspacing=0 cellpadding=4>";
+		echo "<tr><th>Id<th>Origem<th>Descrição<th>Link</tr>";
+		while( ($row = $stmt->fetch()) ){
+			echo "<tr><td>" . $row["Id"] . "</td><td>" . $row["Origem"] . "</td><td>" . $row["Descri"] . "</td><td>" . $row["Link"] . "</td></tr>";
+			}
+		echo "</table>";
+		} catch (PDOException $e) {
+		echo $e->getMessage();
+		}
+?>
